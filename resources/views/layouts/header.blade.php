07b7eca6508d015dd5c0c9239dc8c5e29dd9dc83
bloodpn <ferdi.pn@idn-solution.com>
2017-06-13 12:27:49 +0700

    Header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,9 +4,10 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="container">
-            <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+            <a class="navbar-brand" href="index.html">SpeedCam Polisi Republik Indonesia </a>
             <div class="collapse navbar-collapse" id="navbarExample">
-                <ul class="navbar-nav ml-auto">
+                <img src="">
+                <!-- <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
                         <a class="nav-link" href="about.html">About</a>
                     </li>
@@ -50,7 +51,7 @@
                             <a class="dropdown-item" href="pricing.html">Pricing Table</a>
                         </div>
                     </li>
-                </ul>
+                </ul> -->
             </div>
         </div>
     </nav>
